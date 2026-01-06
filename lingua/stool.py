b7d04eb4135b1247f5c5222b5cbde2b0c86aa2fd
Colin Raffel <craffel@gmail.com>
2026-01-06 16:03:15 +0000

    Fix data copy command

--- lingua/stool.py
+++ lingua/stool.py
@@ -22,7 +22,7 @@ class StoolArgs:
         True  # Wether to copy new code and config and run regardless that dir exists
     )
     override: bool = False  # Wether to delete dump dir and restart
-    nodes: int = 4  # The number of nodes to run the job on.
+    nodes: int = 1  # The number of nodes to run the job on.
     ngpu: int = 8  # The number of GPUs required per node.
     ncpu: int = 16  # The number of CPUs allocated per task.
     mem: str = ""  # The amount of memory to allocate.
@@ -177,7 +177,7 @@ def launch_job(args: StoolArgs):
                 copy_data_command += f"srun --ntasks-per-node=1 s5cmd cp '{data_dir.removesuffix('/')}/{source}/*' {data_root_dir}/{source}\n"
         else:
             for source in data_sources.keys():
-                copy_data_command += f"srun --ntasks-per-node=1 bash -c 'mkdir -p {data_root_dir}/{source} && rsync -arm {data_dir}/{source} {data_root_dir}/{source}'\n"
+                copy_data_command += f"srun --ntasks-per-node=1 bash -c 'mkdir -p {data_root_dir}/{source} && rsync -arm {data_dir}/{source} {data_root_dir}/'\n"
 
     print("Creating directories...")
     os.makedirs(dump_dir, exist_ok=args.dirs_exists_ok or args.override)
