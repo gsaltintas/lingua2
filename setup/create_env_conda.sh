0c26afb334be56143e5426c0a0a24b049022a2fb
Gül Sena Altıntaş <30217912+gsaltintas@users.noreply.github.com>
2026-07-24 14:55:09 -0400

    Added uv setup script

--- setup/create_env_conda.sh
+++ setup/create_env_conda.sh
@@ -14,19 +14,27 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
-module load cuda/12.2
-
 # Start timer
 start_time=$(date +%s)
 
 # Get the current date
 current_date=$(date +%y%m%d)
 
-uv venv --python=3.10
-source .venv/bin/activate
-uv pip install torch==2.5.0 xformers --index-url https://download.pytorch.org/whl/cu121
-uv pip install ninja
-uv sync
+# Create environment name with the current date
+env_prefix=lingua_$current_date
+
+# Create the conda environment
+
+source $CONDA_ROOT/etc/profile.d/conda.sh
+conda create -n $env_prefix python=3.11 -y -c anaconda
+conda activate $env_prefix
+
+echo "Currently in env $(which python)"
+
+# Install packages
+pip install torch==2.5.0 xformers --index-url https://download.pytorch.org/whl/cu121
+pip install ninja
+pip install --requirement requirements.txt
 
 # End timer
 end_time=$(date +%s)
@@ -38,3 +46,5 @@ elapsed_time=$((end_time - start_time))
 elapsed_minutes=$((elapsed_time / 60))
 
 echo "Environment $env_prefix created and all packages installed successfully in $elapsed_minutes minutes!"
+
+
